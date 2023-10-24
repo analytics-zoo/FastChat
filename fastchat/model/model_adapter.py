4c6ed8717ebcbde09e9db088339a0736f75a93f6
gc-fu <guancheng.fu@intel.com>
2023-10-24 15:26:12 +0800

    Fix input prompt error

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -738,7 +738,7 @@ class ChatGLMAdapter(BaseModelAdapter):
         from bigdl.llm.transformers import AutoModel
 
         if use_bigdl_low_bit_weights:
-            model = AutoModel.load_low_bit(model_path)
+            model = AutoModel.load_low_bit(model_path, trust_remote_code=True, **from_pretrained_kwargs)
         else:
             model = AutoModel.from_pretrained(
                 model_path,
